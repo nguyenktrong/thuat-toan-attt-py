4482e80ad3c10a671e6d5faccce959645409268f
Nguyễn Kim Trọng <80269292+nguyenktrong@users.noreply.github.com>
2021-06-09 20:22:37 +0700

    Create 2.1 sàng nguyên tố.py

---
+++ 2.1 sàng nguyên tố.py
@@ -0,0 +1,12 @@
+#thuật toán sàng nguyên tố Eratosthenes
+def snt(n):
+  check=[True]*(n+1)
+  s=[]
+  for i in range(2,n+1):
+    if check[i]:
+      for j in range(i*i,n+1,i):
+        check[j]=False
+      s.append(i)
+  return s
+n=int(input('Nhap n = '))
+print(snt(n))
