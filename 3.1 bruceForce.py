3e89981a53193c6529828ab9034880f1914d9aee
Nguyễn Kim Trọng <80269292+nguyenktrong@users.noreply.github.com>
2021-06-21 10:43:00 +0700

    done !

--- 3.1 bruceForce.py
+++ 3.1 bruceForce.py
@@ -1,11 +1,11 @@
 # đối sánh mẫu-thuật toán vét cạn
 def bruceForce(T,P):
-  operation = 0
-  loop = 0
+  operation = 0 # tính số phép toán
+  loop = 0 # tính số vòng lặp
   for i in range((len(T)-len(P))+1):
     operation += 1
     loop += 1
-    count = 0
+    count = 0 # đếm số ký tự bằng nhau 
     j = 0
     while P[j] == T[i+j]:
       count += 1
@@ -18,13 +18,13 @@ def bruceForce(T,P):
 T = input("Nhap T: ")
 P = input("Nhap P: ")
 if bruceForce(T, P)[0]:
-  print(f"P xuat hien o vi tri {bruceForce(T, P)[1]-1}")
-  print(f"so vong lap = {bruceForce(T,P)[1]}")
-  print(f"so phep toan = {bruceForce(T,P)[2]}")
+  print(f"P xuat hien o vi tri {bruceForce(T, P)[1]-1}") # loop -1
+  print(f"so vong lap = {bruceForce(T,P)[1]}") # loop
+  print(f"so phep toan = {bruceForce(T,P)[2]}") # operation
 else:
   print("P khong xuat hien trong T ")
-  print(f"so vong lap = {bruceForce(T,P)[1]}")
-  print(f"so phep toan = {bruceForce(T,P)[2]}")
+  print(f"so vong lap = {bruceForce(T,P)[1]}") # loop
+  print(f"so phep toan = {bruceForce(T,P)[2]}") # operation
 
 
 
