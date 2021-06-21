0ecfe38b8154e74843393e6a55f72ead0a9777f7
Nguyễn Kim Trọng <80269292+nguyenktrong@users.noreply.github.com>
2021-06-21 10:32:50 +0700

    Update 3.1 bruceForce.py

--- 3.1 bruceForce.py
+++ 3.1 bruceForce.py
@@ -4,21 +4,27 @@ def bruceForce(T,P):
   loop = 0
   for i in range((len(T)-len(P))+1):
     operation += 1
-    loop = i
+    loop += 1
     count = 0
     j = 0
-    if P[j] != T[i]:
-      continue
-    else:
-      while P[j] == T[i]:
-        count += 1
-        i += 1
-        j += 1
-        if count == len(P):
-          return True,loop,operation
-        operation += 1
-  return False
+    while P[j] == T[i+j]:
+      count += 1
+      j += 1
+      if count == len(P):
+        return True, loop, operation
+      operation += 1
+  return False, loop, operation
+
 T = input("Nhap T: ")
 P = input("Nhap P: ")
-print(bruceForce(T,P))
+if bruceForce(T, P)[0]:
+  print(f"P xuat hien o vi tri {bruceForce(T, P)[1]-1}")
+  print(f"so vong lap = {bruceForce(T,P)[1]}")
+  print(f"so phep toan = {bruceForce(T,P)[2]}")
+else:
+  print("P khong xuat hien trong T ")
+  print(f"so vong lap = {bruceForce(T,P)[1]}")
+  print(f"so phep toan = {bruceForce(T,P)[2]}")
+
+
 
