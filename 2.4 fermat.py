6fc0ea769e9377b2d87d644d43ab5b7ce138632c
Nguyễn Kim Trọng <80269292+nguyenktrong@users.noreply.github.com>
2021-07-30 11:10:26 +0700

    done !!!

--- 2.4 fermat.py
+++ 2.4 fermat.py
@@ -1,25 +1,27 @@
 # kiểm tra số nguyên tố lớn- định lý fermat
 from random import randrange
-def convertbin(x):
-  bin = ""
-  while x!=0:
-    bin += str(x%2)
-    x = x//2
-  return bin
+
 def mod(a,n):
-  A=a
-  b=1
-  k = convertbin(n-1)
-  if k[0] == '1':
-    b=a
+  A = a
+  b = 1
+  if n-1 == 0:
+    return b
+  k = bin(n-1)[2:][::-1]
+  if k[0] == "1":
+    b = a
   for i in range(1,len(k)):
-    A = (A*A) % n  
-    if k[i] == '1':    
-      b = (b*A) % n
+    A = A*A % n
+    if k[i] == "1":
+      b = b*A % n
   return b
+
+def fermat(n,t):
+  for i in range(t):
+    a = randrange(2,n-2)
+    if mod(a,n) !=1:
+      return f"{n} la hop so"
+  return f"{n} là snt"
+
 n = int(input("n = "))
-a = randrange(2,n-2)
-if mod(a,n)==1:
-  print("n là snt")
-else:
-  print("n là hợp số")
+t = int(input("t = "))
+print(fermat(n,t))
