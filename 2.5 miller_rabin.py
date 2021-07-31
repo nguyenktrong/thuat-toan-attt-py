addd53bb1a75e5fccce055b762cb0a2fdd44faa3
Nguyễn Kim Trọng <80269292+nguyenktrong@users.noreply.github.com>
2021-07-31 09:22:40 +0700

    done!!!

--- 2.5 miller_rabin.py
+++ 2.5 miller_rabin.py
@@ -1,48 +1,47 @@
 from random import randrange
-def convertbin(x):
-  bin = ""
-  while x!=0:
-    bin += str(x%2)
-    x = x//2
-  return bin
-def mod(a,r,n):
+
+def mod(a,n,r):
   A = a
   b = 1
-  k = convertbin(r)
-  if k[0] == '1':
-    b=a
+  if r == 0:
+    return b
+  k = bin(r)[2:][::-1]
+  if k[0] == "1":
+    b = a
   for i in range(1,len(k)):
-    A = (A*A) % n
-    if k[i] == '1':    
-      b = (b*A) % n
+    A = A*A %n
+    if k[i] == "1":
+      b = A*b % n
   return b
-def miller_rabin(n):  
+
+def miller_rabin(n,t):
   d = n-1
   for i in range(2,n):
     cnt = 0
-    while d%i == 0:
+    while n%i == 0:
+      cnt += 1
       d /= i
-      cnt = cnt + 1
     s = cnt
     r = int(d)
     break
-  for x in range(t+1):
+  for i in range(t):
     a = randrange(2,n-1)
-    y = mod(a,r,n)
-    if y != 1 and y != (n-1):
-      j = 1
-      while j <= (s-1) and y != (n-1):
-        y = (y*y) % n
+    y = mod(a,n,r)
+    if y != 1 and y != n-1:
+      j=1
+      while j <= s-1 and y != n-1:
+        y = y*y % n
         if y == 1:
-          return True
-        j = j+1
-      if y != (n-1):
-        return True
-  return False
-n = int(input("n = "))
-t = int(input("t = "))
-if miller_rabin(n):
-  print(f"{n} la hop so")
-else:
-  print(f"{n} la so nguyen to")
+          return f"{n} la hop so"
+        j += 1
+      if y != n-1:
+        return f"{n} la hop so" 
+  return f"{n} la so nguyen to"
+
+n = int(input("Nhap n = "))
+t = int(input("Nhap t = "))
+print(miller_rabin(n,t))
+
+
+
 
