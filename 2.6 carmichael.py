66e4bce6aef67025379f87973c1474fcdb574b6e
Nguyễn Kim Trọng <80269292+nguyenktrong@users.noreply.github.com>
2021-07-31 09:34:44 +0700

    Update 2.6 carmichael.py

--- 2.6 carmichael.py
+++ 2.6 carmichael.py
@@ -1,11 +1,21 @@
 # số giả nguyên tố carmichael
-from math import gcd
-def carmichael(n):
-  for a in range(2,n+1):
-    if gcd(a,n) == 1:
-      if (a**(n-1))%n != 1:
-        return False
+from math import sqrt,gcd
+def prime(n):
+  for i in range(2, int(sqrt(n))):
+    if n%i == 0:
+      return False
   return True
+
+def carmichael(n):
+  if prime(n):
+    return False
+  else:
+    for a in range(2, n+1):
+      if gcd(a, n) == 1:
+        if (a**(n-1))%n != 1:
+          return False
+    return True
 
 n = int(input("Nhap n = "))
 print(carmichael(n))
+
