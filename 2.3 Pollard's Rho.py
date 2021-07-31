89179d66c69a816f1c80d2fb9a933074e5cb0a8f
Nguyễn Kim Trọng <80269292+nguyenktrong@users.noreply.github.com>
2021-07-31 10:01:53 +0700

    done!!!

--- 2.3 Pollard's Rho.py
+++ 2.3 Pollard's Rho.py
@@ -1,24 +1,22 @@
 # thừa số không tầm thường
-import math
 def gcd(a,b):
-  A=a
-  B=b
-  while B>0:
-    R=A%B
-    A=B
-    B=R
-  return A
+  while b > 0:
+    R = a % b
+    a = b
+    b = R
+  return a
 def Rho(n):
-  a=2
-  b=2
+  a = 2
+  b = 2
   for i in range(n):
-    a=(a*a+1)%n
-    b=(b*b+1)%n
-    b=(b*b+1)%n
-    d=math.gcd(a-b,n)
-    if n > d > 1:
-      return d
-    if n == d:
-      return False
-n=int(input("Nhap n = "))
-print(f"d = {Rho(n)}")
+    a = (a**2 + 1) % n
+    b = (b**2 + 1) % n
+    b = (b**2 + 1) % n
+    d = gcd(a-b, n) 
+    if 1 < d < n:
+      return f"d = {d}"
+    elif d == n:
+      return "Math error"
+
+n = int(input("Nhap n = "))
+print(Rho(n))
