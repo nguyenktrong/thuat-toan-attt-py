1308b5564b00ce4cbb3019eb3d8b0ab459fccd6e
Nguyễn Kim Trọng <80269292+nguyenktrong@users.noreply.github.com>
2021-08-02 10:00:43 +0700

    done!!!

--- 1.3 sum in Fp.py
+++ 1.3 sum in Fp.py
@@ -1,53 +1,50 @@
 # Cộng số nguyên lớn trên trường hữu hạn
-from math import *
-def convert_array(a,w,t):
-  A = []
-  for i in range(t):
-    A.append(a//(2**((t-i-1)*w)))
-    a = a % (2**((t-i-1)*w))
-  return A
+from math import*
+def convert_array(number,w,t):
+  Arr = []
+  for i in range(t-1,-1,-1):
+    Arr.append(number//2**(w*i))
+    number %= 2**(w*i)
+  return Arr
 
-def convert_number(A,w,t):
+def convert_number(Arr,w,t):
   number = 0
   for i in range(t-1,-1,-1):
-    number += A[t-1-i]*(2**(w*i))
+    number += Arr[t-i-1]*2**(w*i)
   return number
-
-def sum(A,B,w,t):
-  C = []
+  
+def sum(arr_A,arr_B,w,t):
+  arr_C = []
   e = 0
   for i in range(t-1,-1,-1):
-    c = A[i] + B[i] + e
-    if 0 <= c < 2**w:
-      e = 0
-    else:
-      e = 1
-    C.append(c%(2**w))
-  return e,C[::-1]
+    c = arr_A[i] + arr_B[i] + e
+    e = 0 if 2**w > c >= 0 else 1
+    arr_C.append(c%2**w)
+  return e,arr_C[::-1]  #reverse arr_C
 
-def subtract(A,B,w,t):
-  C = []
+def sub(arr_A,arr_B,w,t):
+  arr_C = []
   e = 0
   for i in range(t-1,-1,-1):
-    c = A[i] - B[i] - e
-    if 0 <= c < 2**w:
-      e = 0
-    else:
-      e = 1
-    C.append(c%(2**w))
-  return e,C[::-1]
-def sumFp(A,B,w,t):
-  if sum(A, B, w, t)[0] == 1:
-    return subtract(sum(A, B, w, t)[1], convert_array(p, w, t), w, t)
+    c = arr_A[i] - arr_B[i] - e
+    e = 0 if 2**w > c >= 0 else 1
+    arr_C.append(c%2**w) 
+  return e,arr_C[::-1]
+
+def sum_Fp(arr_A,arr_B,w,t):
+  e, arr_C = sum(arr_A,arr_B,w,t)
+  arr_p = convert_array(p,w,t)
+  if e == 1:
+    return sub(arr_C,arr_p,w,t)
   else:
-    if convert_number(sum(A,B,w,t)[1],w,t) > p:
-      return subtract(sum(A, B, w, t)[1], convert_array(p, w, t), w, t)
-    return sum(A, B, w, t)
+    if convert_number(arr_C,w,t) > p:
+      return sub(arr_C,arr_p,w,t)
+  return e,arr_C
+
+A = [int(x) for x in input("Nhap A = ").split()]
+B = [int(y) for y in input("Nhap B = ").split()]
 w = int(input("Nhap W = "))
 p = 2147483647
 m = ceil(log(p,2))
 t = ceil(m/w)
-A = [int(a) for a in input("Nhap A = ").split()]
-B = [int(b) for b in input("Nhap B = ").split()]
-print(sumFp(A,B,w,t))
-
+print(sum_Fp(A,B,w,t))
